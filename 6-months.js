c85c6fb2400b1bc5a77779444d13ffcac7193eaf
Jim Cramer <remarcmij@gmail.com>
2018-02-01 23:09:35 +0100

    updated version 6

--- 6-months.js
+++ 6-months.js
@@ -2,3 +2,44 @@
 
 'use strict';
 
+class Month {
+  constructor(name, days) {
+    this.name = name;
+    this.days = days;
+  }
+
+  hasDays(days) {
+    return this.days === days;
+  }
+
+  isLongMonth() {
+    return this.hasDays(31);
+  }
+
+  toString() {
+    return `${this.name} has ${this.days} days.`;
+  }
+
+  toConsole() {
+    console.log(this.toString());
+  }
+}
+
+const months = [
+  new Month('January', 31),
+  new Month('February', 28),
+  new Month('March', 31),
+  new Month('April', 30),
+  new Month('May', 31),
+  new Month('June', 30),
+  new Month('July', 31),
+  new Month('August', 31),
+  new Month('September', 30),
+  new Month('October', 31),
+  new Month('November', 30),
+  new Month('December', 31)
+];
+
+months
+  .filter(month => month.isLongMonth())
+  .forEach(month => month.toConsole());
